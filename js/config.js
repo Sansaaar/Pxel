9393d313a1d92ba8afe2dc7fd457a9319f875c66
Sansaaar <buisness.sansaar@gmail.com>
2026-08-15 16:06:32 +0545

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@ const API_BASE =
     location.hostname === "localhost" ||
     location.hostname === "127.0.0.1"
         ? "http://localhost:3000"
-        : "https://pixel-amber-six.vercel.app/";
+        : "https://pixel-amber-six.vercel.app";
