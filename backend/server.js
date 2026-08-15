59490527a51d193f1b3e5497f24f664f95111c51
Sansaaar <buisness.sansaar@gmail.com>
2026-08-15 15:45:58 +0545

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,29 +14,16 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-
     console.log(req.method, req.url);
-
     next();
-
 });
 
 app.use("/api/chat", chatRoute);
 
 app.get("/", (req, res) => {
-
     res.json({
-
         status: "Pixel Backend Running 🚀"
-
     });
-
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-
-    console.log(`Server running on ${PORT}`);
-
-});
+module.exports = app;
